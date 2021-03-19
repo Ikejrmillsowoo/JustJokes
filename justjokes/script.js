ebb1a202b735dd08f84b00d820982a977025207f
poppymills2@gmail.com <poppymills2@gmail.com>
2021-03-19 15:42:40 -0400

    updated jokes

---
+++ justjokes/script.js
@@ -0,0 +1,18 @@
+const display = () => {
+  const btn = document.getElementById("button");
+  const reset = document.getElementById("reset");
+  const answer = document.getElementById("response");
+
+  btn.style.display = "block";
+  reset.style.display = "inline";
+  answer.style.display = "none";
+};
+
+fetch("https://official-joke-api.appspot.com/random_joke")
+  .then((res) => res.json())
+  .then((data) => {
+    return (
+      (document.getElementById("rainbow").innerHTML = data.setup),
+      (document.getElementById("button").innerHTML = data.punchline)
+    );
+  });
